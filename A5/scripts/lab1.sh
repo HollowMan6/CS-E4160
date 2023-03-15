7260a98932c0b40b797b98a24b9035f14755f432
Hollow Man <hollowman@opensuse.org>
2023-03-15 21:46:03 +0200

    A5
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- A5/scripts/lab1.sh
+++ A5/scripts/lab1.sh
@@ -29,3 +29,43 @@ Host lab3
 EOL
 
 chmod 600 ~/.ssh/*
+
+sudo modprobe ip_conntrack_ftp
+sudo apt install -y squid
+
+# # Create a table and a chain for the FORWARD hook with nftables:
+# sudo nft add table inet filter
+# sudo nft add chain inet filter forward { type filter hook forward priority 0 \; policy drop \; }
+# # This will create a table named filter and a chain named forward that will drop all packets by default.
+
+# # Allow ping from lab2 on the enp0s8 interface and replies to lab2:
+# sudo nft add rule inet filter forward iif enp0s8 icmp type echo-request counter accept
+# sudo nft add rule inet filter forward oif enp0s8 icmp type echo-reply counter accept
+
+# # Allow TCP packets with destination port 22 (SSH) from lab2 to initiate new or established connections, and TCP packets
+# # with source port 22 from lab2 to continue established connections.
+# sudo nft add rule inet filter forward iif enp0s8 tcp dport 22 ct state new,established counter accept
+# sudo nft add rule inet filter forward oif enp0s8 tcp sport 22 ct state established counter accept
+
+# # Allow TCP packets with destination port 80 (HTTP) or 443 (HTTPS) from lab2 to initiate
+# # new or established connections, and TCP packets with source port 80 or 443 from lab2 to
+# # continue established connections.
+# sudo nft add rule inet filter forward iif enp0s8 tcp dport 80 ct state new,established counter accept
+# sudo nft add rule inet filter forward oif enp0s8 tcp sport 80 ct state established counter accept
+
+# # Allow TCP packets with destination port 20 (FTP data) or 21 (FTP control) from lab2 to initiate new or established connections,
+# # and TCP packets with source port 20 or 21 from lab2 to continue established connections.
+# sudo nft add rule inet filter forward iif enp0s8 tcp dport {20-21} ct state new,established counter accept
+# sudo nft add rule inet filter forward oif enp0s8 tcp sport {20-21} ct state established counter accept
+
+# sudo iptables -t nat -A PREROUTING -s lab2 -p tcp --dport 80 -j REDIRECT --to-port 3128
+# sudo tee -a /etc/squid/squid.conf <<EOL
+# http_port 3128 transparent
+# acl lab2 src 192.168.0.3/32    # IP address of lab2
+
+# http_access allow lab2
+# http_access deny all
+# EOL
+# sudo systemctl restart squid
+
+
